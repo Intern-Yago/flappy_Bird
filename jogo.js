1d7cb4d77d88a668b7100773597b1e32e4d413ca
Syri-Yagowo <yagovictorbotafogo@gmail.com>
2021-04-21 08:00:46 -0300

    objeto background

--- jogo.js
+++ jogo.js
@@ -40,3 +40,42 @@ const chao = {
     }
 }
 
+const chao = {
+    sx: 0,
+    sy: 610,
+    W: 224,
+    H: 112,
+    dx: 0,
+    dy: canvas.height - 112,
+    desenhar(incremento=0){
+        contexto.drawImage(
+            sprites,
+            chao.sx, chao.sy,
+            chao.W, chao.H,
+            chao.dx + incremento, chao.dy,
+            chao.W, chao.H,
+        )
+    }
+}
+
+const back = {
+    sx:390,
+    sy:0,
+    W:275,
+    H:204,
+    dx:0,
+    dy:canvas.height - 204,
+    desenhar(incremento=0, fundo=true){
+        if (fundo == true){
+        contexto.fillStyle = '#70c5ce';
+        contexto.fillRect(0,0, canvas.width, canvas.height)
+        }
+        contexto.drawImage(
+            sprites,
+            back.sx, back.sy,
+            back.W, back.H,
+            back.dx + incremento, back.dy,
+            back.W, back.H,
+        )
+    }
+}
