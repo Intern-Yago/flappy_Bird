4d1018ba728c9bd5249fe93b49a602a0a20248a4
Syri-Yagowo <yagovictorbotafogo@gmail.com>
2021-04-21 08:00:07 -0300

    objeto chao

--- jogo.js
+++ jogo.js
@@ -22,3 +22,21 @@ const flappy = {
     }
 }
 
+const chao = {
+    sx: 0,
+    sy: 610,
+    W: 224,
+    H: 112,
+    dx: 0,
+    dy: canvas.height - 112,
+    desenhar(incremento=0){
+        contexto.drawImage(
+            sprites,
+            chao.sx, chao.sy,
+            chao.W, chao.H,
+            chao.dx + incremento, chao.dy,
+            chao.W, chao.H,
+        )
+    }
+}
+
