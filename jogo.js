5d5f313d70f7d05225f134cbf37e50806333e2f5
Syri-Yagowo <yagovictorbotafogo@gmail.com>
2021-04-21 07:59:36 -0300

    objeto Flappy

--- jogo.js
+++ jogo.js
@@ -1,1 +1,24 @@
-const 
+const sprites = new Image()
+sprites.src = "./sprites.png"
+
+const canvas = document.querySelector('canvas')
+const contexto = canvas.getContext('2d')
+
+const flappy = {
+    sx: 0,
+    yx: 0,
+    W: 33,
+    H: 24,
+    dx: 10, 
+    dy: 50,
+    desenhar(){
+        contexto.drawImage(
+            sprites,
+            flappy.sx, flappy.yx,
+            flappy.W, flappy.H,
+            flappy.dx, flappy.dy,
+            flappy.W, flappy.H,
+        )   
+    }
+}
+
